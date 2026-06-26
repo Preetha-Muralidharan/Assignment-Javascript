e47e463fcfda1d5cd2e060ad311624581d65947a
Preetha-Muralidharan <preetha.muralidharan19@gmail.com>
2026-06-26 13:50:04 +0530

    Create DataType.js

---
+++ DataType.js
@@ -0,0 +1,11 @@
+let userName="Preetha"
+let organization="ABC"
+let contactNumber=978456321
+let knowsAutomation=false
+let usesPlaywright
+
+console.log(typeof(userName))
+console.log(typeof(organization))
+console.log(typeof(contactNumber))
+console.log(typeof(knowsAutomation))
+console.log(typeof(usesPlaywright))
